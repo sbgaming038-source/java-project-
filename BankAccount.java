95011fa82e954c12e6f51c6f1f1083b916802f76
sbgaming038-source <sbgaming038@gmail.com>
2026-03-05 09:21:26 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,24 @@
+public class BankAccount {
 
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if(amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
